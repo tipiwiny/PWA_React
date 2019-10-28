f29e980afe906001b5a84c573ec31bb0e76cb6db
Juan Luis Perez <tipiwiny@gmail.com>
2019-10-28 23:19:00 +0100

    Add chat with FireBase

--- src/components/App.js
+++ src/components/App.js
@@ -7,9 +7,28 @@ import {Route, withRouter} from 'react-router-dom';
 
 import './app.css';
 class App extends Component {
-  statate = {
-    user: null
+  state = {
+    user: null,
+    messages: [],
+    messagesLoaded: false
   }
+  handleSubmitMessage = msg => {
+    const data = {
+      msg,
+      author: this.state.user.email,
+      user_id: this.state.user.uid,
+      timestamp: Date.now()
+    };
+    Firebase.database().ref('messages/').push(data);
+  }
+  onMessage = snapshot => {
+    const messages = Object.keys(snapshot.val()).map(key => {
+      const msg = snapshot.val()[key];
+      msg.id = key;
+      return msg;
+    });
+    this.setState({messages});
+  };
   componentDidMount() {
     Firebase.auth().onAuthStateChanged((user) => {
       if (user) {
@@ -18,12 +37,18 @@ class App extends Component {
         this.props.history.push('/login');
       }
     });
+    Firebase.database().ref('/messages').on('value', snapshot => {
+      this.onMessage(snapshot);
+      if (!this.state.messagesLoaded) {
+        this.setState({messagesLoaded: true});
+      }
+    });
   }
   render() {
     return (<div id="container" className="inner-container">
-      <Route exact path="/" component={ChatContainer}/>
+      <Route exact={true} path="/" render={() => (<ChatContainer messagesLoaded={this.state.messagesLoaded} messages={this.state.messages} user={this.state.user} onSubmit={this.handleSubmitMessage}/>)}/>
       <Route path="/login" component={LoginContainer}/>
-       <Route path="/users/:id" component={UserContainer} />
+      <Route path="/users/:id" render={({history, match}) => (<UserContainer messages={this.state.messages} messagesLoaded={this.state.messagesLoaded} userID={match.params.id}/>)}/>
     </div>);
   }
 }
